57652bf64c83911906b0562b586b01da46989a95
Kacper S <kacpersmal@gmail.com>
2024-07-12 16:49:45 +0200

    chore: Add Swagger documentation to API

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Spotify Tracker API')
+    .setDescription('Official API for Spotify Tracker App')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(3001);
+  console.log(`Server is running on ${await app.getUrl()}`)
 }
 bootstrap();
